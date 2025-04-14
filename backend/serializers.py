674bedd606743391c25089ae913570a97aa8f24d
Jose Mojica Perez <jlmojicaperez@gmail.com>
2025-04-13 22:58:10 -0400

    Added different serializers for when models are in lists

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,38 +1,62 @@
 from flask_restful import fields
 
+countries_fields = {
+    "country_id": fields.Integer,
+    "name": fields.String,
+}
+
+genders_fields = {
+    "gender_id": fields.Integer,
+    "name": fields.String,
+}
+
+tags_fields = {
+    "tag_id": fields.Integer,
+    "name": fields.String,
+}
+
+composers_fields = {
+    "composer_id": fields.Integer,
+    "first_name": fields.String,
+    "last_name": fields.String,
+    "image_url": fields.String,
+}
+
 composer_fields = {
     "composer_id": fields.Integer,
     "first_name": fields.String,
     "last_name": fields.String,
     "image_url": fields.String,
-    "country_of_birth_id": fields.Integer,
-    "country_of_birth": fields.String,
     "birth_date": fields.DateTime(dt_format="iso8601"),
     "death_date": fields.DateTime(dt_format="iso8601"),
-    "gender_id": fields.Integer,
-    "gender": fields.String,
-    "country_of_education_id": fields.Integer,
-    "country_of_education": fields.String,
+    "country_of_birth": fields.Nested(countries_fields),
+    "country_of_education": fields.Nested(countries_fields),
+    "gender": fields.Nested(genders_fields),
     "sample_url": fields.String,
     "sample_title": fields.String,
     "website": fields.String,
     "email": fields.String,
-    "more_info": fields.String
-    }
+    "more_info": fields.String,
+    "tags": fields.List(cls_or_instance=fields.Nested(tags_fields))
+}
 
 tag_fields = {
     "tag_id": fields.Integer,
     "name": fields.String,
-    "description": fields.String
+    "description": fields.String,
+    "composers": fields.List(fields.Nested(composers_fields))
 }
 
 country_fields = {
     "country_id": fields.Integer,
     "name": fields.String,
+    "composers_born": fields.List(cls_or_instance=fields.Nested(composers_fields)),
+    "composers_educated": fields.List(cls_or_instance=fields.Nested(composers_fields))
 }
 
 gender_fields = {
     "gender_id": fields.Integer,
     "name": fields.String,
+    "composers": fields.List(fields.Nested(composers_fields))
 }
 
